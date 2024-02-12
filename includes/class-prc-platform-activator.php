0683290efe989d738386a867b66a48730bd371d6
prcdevgitbot <webdev@pewresearch.org>
2024-02-12 15:15:06 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/0abcac5962c87f71528c7281a85b9ed440f335a5 from refs/heads/release/5.0

--- includes/class-prc-platform-activator.php
+++ includes/class-prc-platform-activator.php
@@ -3,16 +3,12 @@ use DEFAULT_TECHNICAL_CONTACT;
 
 class PRC_Platform_Activator {
 	public static function activate() {
-		require_once plugin_dir_path(__FILE__) . 'class-prc-platform-content-bootstrapper.php';
 
 		wp_mail(
 			DEFAULT_TECHNICAL_CONTACT,
 			'PRC Platform Activated 🚀',
 			'The PRC Platform has been activated on ' . get_site_url()
 		);
-
-		$bootstrapper = new PRC_Platform_Content_Bootstrapper();
-		$bootstrapper->run();
 
 		flush_rewrite_rules();
 	}
