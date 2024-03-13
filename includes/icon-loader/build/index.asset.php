ec388eca4a3ae9210f935dbb67699d6da84434c4
prcdevgitbot <webdev@pewresearch.org>
2024-03-13 22:35:12 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/801c07b1bdcb7dc27c6bd02e1ca2b6a26da5072b from refs/heads/release/5.0

--- includes/icon-loader/build/index.asset.php
+++ includes/icon-loader/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-polyfill'), 'version' => '4c6054cae2f181e63b65');
+<?php return array('dependencies' => array('react', 'wp-polyfill'), 'version' => 'f1df0f4c3f8ff4db94ab');
