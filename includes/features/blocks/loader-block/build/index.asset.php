b73c5ef3368995a0f2d8194253848f3024d53c40
prcdevgitbot <webdev@pewresearch.org>
2024-03-25 19:30:44 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/b5f993a04e302733483749e89cbdc15b0bd3a98a from refs/heads/release/5.0

--- includes/features/blocks/loader-block/build/index.asset.php
+++ includes/features/blocks/loader-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('prc-components', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '9331835ca6afda8fc772');
+<?php return array('dependencies' => array('prc-components', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'e1dd1ac711411d4fd3fa');
