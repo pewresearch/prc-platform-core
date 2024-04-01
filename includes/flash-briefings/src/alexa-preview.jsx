752dff33895682996973712cd4a0c61c287bd9c7
prcdevgitbot <webdev@pewresearch.org>
2024-04-01 17:52:06 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/4e4f852e1e0464bb3ddf5bda51152a0999bcd5b7 from refs/heads/release/5.0

--- includes/flash-briefings/src/alexa-preview.jsx
+++ includes/flash-briefings/src/alexa-preview.jsx
@@ -5,68 +5,64 @@ import { PanelBody, PanelRow, Button } from '@wordpress/components';
 const SECURITY_TOKEN = ''; // @TODO need to add this to env vars @benwormald
 
 const addMedia = (content, postId) => {
-    const mediaCollection = new wp.api.collections.Media();
-    const fetched = mediaCollection
-        .fetch({ data: { parent: postId } })
-        .then((res) => {
-            if (res.length > 0) {
-                //delete results as model
-                res.forEach((mediaObj) => {
-                    let mediaModel = new wp.api.models.Media({
-                        id: mediaObj.id,
-                    });
-                    mediaModel.fetch();
-                    mediaModel.destroy();
-                });
-            }
-            uploadMedia({
-                filesList: [
-                    new File([content], `$brief-${postId}-${Date.now()}.mp3`, {
-                        type: 'audio/mpeg',
-                    }),
-                ],
-                additionalData: {
-                    post: postId,
-                    foo: 'bar',
-                },
-                onFileChange: ([fileObj]) => {
-                    document.getElementById('audio-source').src = fileObj.url;
-                    document.getElementById('audio-el').load();
-                },
-                onError: console.error,
-            });
-        });
+	const mediaCollection = new wp.api.collections.Media();
+	const fetched = mediaCollection
+		.fetch({ data: { parent: postId } })
+		.then((res) => {
+			if (res.length > 0) {
+				// delete results as model
+				res.forEach((mediaObj) => {
+					const mediaModel = new wp.api.models.Media({
+						id: mediaObj.id,
+					});
+					mediaModel.fetch();
+					mediaModel.destroy();
+				});
+			}
+			uploadMedia({
+				filesList: [
+					new File([content], `$brief-${postId}-${Date.now()}.mp3`, {
+						type: 'audio/mpeg',
+					}),
+				],
+				additionalData: {
+					post: postId,
+					foo: 'bar',
+				},
+				onFileChange: ([fileObj]) => {
+					document.getElementById('audio-source').src = fileObj.url;
+					document.getElementById('audio-el').load();
+				},
+				onError: console.error,
+			});
+		});
 };
 
 async function postData(url, data) {
-    const res = await fetch(url, {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
-    const content = await res.blob();
-    return content;
+	const res = await fetch(url, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	});
+	const content = await res.blob();
+	return content;
 }
 
 const AlexaPreview = ({ blockContent }) => {
-    return (
+	return (
 		<PanelBody title={__('Alexa Preview', 'alexa-preview')}>
 			<PanelRow>
 				{__(
-					'A preview of the audio generated for the Amazon Alexa "Daily Briefing" skill.',
-					'alexa-preview',
+					'A preview of the audio generated for the Amazon Alexa "Flash Briefing" skill.',
+					'alexa-preview'
 				)}
 			</PanelRow>
 			<PanelRow>
 				<audio id="audio-el" controls>
-					<source
-						id="audio-source"
-						src=""
-						type="audio/mpeg"
-					/>
+					<source id="audio-source" src="" type="audio/mpeg" />
 					Your browser does not support the audio element.
 				</audio>
 			</PanelRow>
@@ -80,11 +76,9 @@ const AlexaPreview = ({ blockContent }) => {
 						postData(
 							'https://polly.pewresearch.io/speak?voice=Joanna',
 							{
-								TEXT:
-									blockContent +
-									' For more, visit pew research dot org.',
+								TEXT: `${blockContent} For more, visit pew research dot org.`,
 								SECURITY_TOKEN,
-							},
+							}
 						).then((data) => {
 							addMedia(data, postId);
 						});
@@ -95,7 +89,7 @@ const AlexaPreview = ({ blockContent }) => {
 				</Button>
 			</PanelRow>
 		</PanelBody>
-    );
+	);
 };
 
 export default AlexaPreview;
