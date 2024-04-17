73ebb8bb298dab6905e0342a24aa707b5402e5d6
prcdevgitbot <webdev@pewresearch.org>
2024-04-17 14:25:53 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/7ea34dbf5c1bb16d38c7707544eac0cd8cea6d49 from refs/heads/release/1.0

--- includes/taxonomies/class-decoded-category.php
+++ includes/taxonomies/class-decoded-category.php
@@ -9,29 +9,27 @@ class Decoded_Category extends Taxonomies {
 	}
 
 	public function register() {
-		$taxonomy_name = self::$taxonomy;
-
 		$labels     = array(
-			'name'                       => _x( 'Decoded Topic', 'Taxonomy General Name', 'text_domain' ),
-			'singular_name'              => _x( 'Decoded Topic', 'Taxonomy Singular Name', 'text_domain' ),
-			'menu_name'                  => __( 'Decoded Topic', 'text_domain' ),
-			'all_items'                  => __( 'All Decoded Topics', 'text_domain' ),
-			'parent_item'                => __( 'Parent Decoded Topic', 'text_domain' ),
-			'parent_item_colon'          => __( 'Parent Decoded Topic:', 'text_domain' ),
-			'new_item_name'              => __( 'New Decoded Topic', 'text_domain' ),
-			'add_new_item'               => __( 'Add Decoded Topic', 'text_domain' ),
-			'edit_item'                  => __( 'Edit Decoded Topic', 'text_domain' ),
-			'update_item'                => __( 'Update Decoded Topic', 'text_domain' ),
-			'view_item'                  => __( 'View Decoded Topic', 'text_domain' ),
-			'separate_items_with_commas' => __( 'Separate Decoded Topic'. ' with commas', 'text_domain' ),
-			'add_or_remove_items'        => __( 'Add or remove Decoded Topic', 'text_domain' ),
+			'name'                       => _x( 'Decoded Category', 'Taxonomy General Name', 'text_domain' ),
+			'singular_name'              => _x( 'Decoded Category', 'Taxonomy Singular Name', 'text_domain' ),
+			'menu_name'                  => __( 'Decoded Category', 'text_domain' ),
+			'all_items'                  => __( 'All Decoded Categories', 'text_domain' ),
+			'parent_item'                => __( 'Parent Decoded Category', 'text_domain' ),
+			'parent_item_colon'          => __( 'Parent Decoded Category:', 'text_domain' ),
+			'new_item_name'              => __( 'New Decoded Category', 'text_domain' ),
+			'add_new_item'               => __( 'Add Decoded Category', 'text_domain' ),
+			'edit_item'                  => __( 'Edit Decoded Category', 'text_domain' ),
+			'update_item'                => __( 'Update Decoded Category', 'text_domain' ),
+			'view_item'                  => __( 'View Decoded Category', 'text_domain' ),
+			'separate_items_with_commas' => __( 'Separate Decoded Category'. ' with commas', 'text_domain' ),
+			'add_or_remove_items'        => __( 'Add or remove Decoded Category', 'text_domain' ),
 			'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
-			'popular_items'              => __( 'Popular Decoded Topic', 'text_domain' ),
-			'search_items'               => __( 'Search Decoded Topic', 'text_domain' ),
+			'popular_items'              => __( 'Popular Decoded Category', 'text_domain' ),
+			'search_items'               => __( 'Search Decoded Category', 'text_domain' ),
 			'not_found'                  => __( 'Not Found', 'text_domain' ),
-			'no_terms'                   => __( 'No Decoded Topic', 'text_domain' ),
-			'items_list'                 => __( 'Decoded Topic list', 'text_domain' ),
-			'items_list_navigation'      => __( 'Decoded Topic list navigation', 'text_domain' ),
+			'no_terms'                   => __( 'No Decoded Category', 'text_domain' ),
+			'items_list'                 => __( 'Decoded Category list', 'text_domain' ),
+			'items_list_navigation'      => __( 'Decoded Category list navigation', 'text_domain' ),
 		);
 
 		$args = array(
@@ -42,6 +40,7 @@ class Decoded_Category extends Taxonomies {
 			'show_admin_column' => true,
 			'show_in_nav_menus' => false,
 			'show_tagcloud'     => false,
+			'show_in_rest'      => true,
 		);
 
 		register_taxonomy( self::$taxonomy, 'decoded', $args );
