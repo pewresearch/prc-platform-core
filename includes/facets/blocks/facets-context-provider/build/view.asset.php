ad9954969c3100410c7fa8f5a90e406ce35522ae
prcdevgitbot <webdev@pewresearch.org>
2024-04-17 20:57:29 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/17f9425004f94ccbd6895a25e181fe3d25e38100 from refs/heads/release/1.0

--- includes/facets/blocks/facets-context-provider/build/view.asset.php
+++ includes/facets/blocks/facets-context-provider/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', 'wp-polyfill', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => '3e0d2bfe4f49b0ba751b', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity', 'wp-polyfill', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => 'da73a74b08454777cf39', 'type' => 'module');
